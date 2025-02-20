681220a6ba8fdcbdb4b3be09ad6bd6eafe88d629
YOGEESH K P <yogeesh793@gmail.com>
2025-02-20 07:12:07 +0530

    Add files via upload

---
+++ Identity.py
@@ -0,0 +1,24 @@
+# Puthon Identity Operators
+x=10
+y=5
+if x is y:
+    print("x and y are same")
+else:
+    print("x and y are not same")
+
+#2
+z=20
+n=30
+if z is n:
+    print("z and n are same")
+else:
+    print("z and n are not same")
+
+#3
+a=10
+b=10
+if a is b:
+    print("a and b are same")
+else:
+    print("a and b are not same")
+    
